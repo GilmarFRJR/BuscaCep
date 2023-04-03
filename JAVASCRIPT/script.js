041a5e9a1f5453d298049b2b733154eb6301b65e
GilmarFRJR <gilmarfrj@gmail.com>
2023-04-02 23:26:56 -0300

    tudo

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,43 @@
+$('#cep').on('blur', function(){
+
+    var CEP = $(this).val()
+    var CEPdigitos = $(this).val().length
+    
+    if (CEPdigitos == 0){
+        return
+    } else
+    if (CEPdigitos < 8 || CEPdigitos > 8){
+
+        alert("CEP inválido")
+        CEP = $(this).val('')
+        return
+
+    } else{
+        $.getJSON(`http://viacep.com.br/ws/${CEP}/json/`, function(infos) {
+
+    if (infos.erro){
+        
+        alert("Esse CEP não existe...")
+        $('#cep').val('')
+
+    } else {
+    $('#endereco').val(infos.logradouro)
+    $('#bairro').val(infos.bairro)
+    $('#cidade').val(infos.localidade)
+    $('#estado').val(infos.uf)
+
+   }})
+
+}})
+
+
+
+
+
+
+
+
+
+  $('button').on('click', function(){
+    location.reload()
+  })
